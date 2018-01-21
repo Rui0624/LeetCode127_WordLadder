b5d97f5628b167fcb411185119a0f97f59399016
Rui <rui@dhcp88.merusouth120.iit.edu>
2018-01-20 22:24:12 -0600

    first commit

--- LeetCode127_WordLadder/main.cpp
+++ LeetCode127_WordLadder/main.cpp
@@ -13,3 +13,36 @@ int main(int argc, const char * argv[]) {
     std::cout << "Hello, World!\n";
     return 0;
 }
+
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> dict (wordList.begin(), wordList.end());
+        unordered_map<string, int> m;
+        m[beginWord] = 1;
+        queue<string> q;
+        q.push(beginWord);
+        
+        while(!q.empty())
+        {
+            string word = q.front();
+            q.pop();
+            for(int i = 0; i < word.size(); i++)
+            {
+                string newWord = word;
+                for(char ch = 'a'; ch <= 'z'; ch++)
+                {
+                    newWord[i] = ch;
+                    if(dict.count(newWord) && newWord == endWord)
+                        return m[word] + 1;
+                    if(dict.count(newWord) && !m.count(newWord)){
+                        m[newWord] = m[word] + 1;
+                        q.push(newWord);
+                    }
+                }
+            }
+        }
+        
+        return 0;
+    }
+};
